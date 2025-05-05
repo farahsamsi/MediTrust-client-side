9f43657dd7d4589c01c08f0f654b9b10b20f304d
farahsamsi <1827034farahsamsiprome@gmail.com>
2025-05-05 23:31:56 +0600

    project setup installing react router, tailwind CSS and daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl text-red-900">Vite + React</h1>
+      <button className="btn btn-primary">Click</button>
+    </>
+  );
+}
+
+export default App;
