0b54079366ee4b8186986a3b8fb242918329a86b
farahsamsi <1827034farahsamsiprome@gmail.com>
2025-05-26 15:10:59 +0600

    made seller dashboard home page dynamic with data from server side

--- src/Pages/DashboardPages/Seller/SellerHome.jsx
+++ src/Pages/DashboardPages/Seller/SellerHome.jsx
@@ -1,7 +1,51 @@
 import { FaDollarSign, FaClock, FaCheckCircle } from "react-icons/fa";
 import DashboardBanner from "../../Shared/DashboardBanner";
+import useAllOrders from "../../../Hooks/useAllOrders";
+import useAuth from "../../../Hooks/useAuth";
+import { useEffect, useState } from "react";
 
 const SellerHome = () => {
+  const [allOrders] = useAllOrders();
+  const { user } = useAuth();
+  const [totalPaid, setTotalPaid] = useState();
+  const [totalPending, setTotalPending] = useState();
+
+  useEffect(() => {
+    const paidMedicineArray = [];
+    allOrders
+      ?.filter((order) => order?.paymentStatus === "paid")
+      ?.map((order) => paidMedicineArray?.push(...(order?.order?.items || {})));
+
+    const filteredSellerPaid = paidMedicineArray?.filter(
+      (item) => item?.sellerEmail === user.email
+    );
+
+    const sum = filteredSellerPaid?.reduce((acc, order) => {
+      return acc + (order?.totalPrice || 0);
+    }, 0);
+
+    setTotalPaid(sum);
+  }, [allOrders, user.email]);
+
+  useEffect(() => {
+    const pendingMedicineArray = [];
+    allOrders
+      ?.filter((order) => order?.paymentStatus === "pending")
+      ?.map((order) =>
+        pendingMedicineArray?.push(...(order?.order?.items || {}))
+      );
+
+    const filteredSellerPaid = pendingMedicineArray?.filter(
+      (item) => item?.sellerEmail === user.email
+    );
+
+    const sum = filteredSellerPaid?.reduce((acc, order) => {
+      return acc + (order?.totalPrice || 0);
+    }, 0);
+
+    setTotalPending(sum);
+  }, [allOrders, user.email]);
+
   return (
     <section className="w-full px-1">
       <DashboardBanner
@@ -15,7 +59,7 @@ const SellerHome = () => {
           <FaCheckCircle className="text-green-600 text-4xl" />
           <div>
             <h3 className="text-xl font-semibold text-green-700">Paid Total</h3>
-            <p className="text-2xl font-bold text-green-900">$3,240.50</p>
+            <p className="text-2xl font-bold text-green-900">Tk {totalPaid}</p>
           </div>
         </div>
 
@@ -26,7 +70,9 @@ const SellerHome = () => {
             <h3 className="text-xl font-semibold text-yellow-700">
               Pending Total
             </h3>
-            <p className="text-2xl font-bold text-yellow-900">$1,120.00</p>
+            <p className="text-2xl font-bold text-yellow-900">
+              Tk {totalPending}
+            </p>
           </div>
         </div>
       </div>
